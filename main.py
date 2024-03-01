880aef33857679a0de7337bfb306dc220773bcb4
Jinkyiyer <161192592+Jinkyiyer@users.noreply.github.com>
2024-03-01 15:00:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -138,6 +138,9 @@ def submit():
             return f"Error: {err}"
 
 
+from app import app
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000 , debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
 
